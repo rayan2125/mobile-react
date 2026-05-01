0502850675c056e32fbd197e38c90df296059b38
rayan2125 <dhananjay.s.dubey20125@gmail.com>
2026-05-01 11:49:23 +0530

    auth screen

--- src/navigation/rootNavigation.js
+++ src/navigation/rootNavigation.js
@@ -1,11 +1,13 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import OnBoardingScreen from "../screen/onBoardingScreen";
+import SignupScreen from "../screen/auth/signupScreen";
 
 const Stack = createNativeStackNavigator();
 const RootNavigation = () => {
     return (
         <Stack.Navigator>
             <Stack.Screen  options={{headerShown:false}} name="OnBoardingScreen" component={OnBoardingScreen} />
+            <Stack.Screen  options={{headerShown:false}} name="SignupScreen" component={SignupScreen} />
         </Stack.Navigator>
     )
 };
---
+++ src/screen/auth/signupScreen.js
@@ -0,0 +1,10 @@
+import { View } from "react-native"
+
+const SignupScreen=()=>{
+    return(
+        <View>
+            <Text>SignupScreen</Text>
+        </View>
+    )   
+};
+export default SignupScreen;
